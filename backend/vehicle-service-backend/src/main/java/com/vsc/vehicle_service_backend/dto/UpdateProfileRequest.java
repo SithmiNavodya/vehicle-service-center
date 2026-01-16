c6d5e0776442fec7c057ea99ad74f582df1cdb35
SithmiNavodya <navodyathilakarathna13@gmail.com>
2026-01-16 12:55:16 +0530

    Resolve compilation errors

--- backend/vehicle-service-backend/src/main/java/com/vsc/vehicle_service_backend/dto/UpdateProfileRequest.java
+++ backend/vehicle-service-backend/src/main/java/com/vsc/vehicle_service_backend/dto/UpdateProfileRequest.java
@@ -1,9 +1,9 @@
-package com.vehicleservice.dto;
+package com.vsc.vehicle_service_backend.dto;
 
 import lombok.Data;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Data
 public class UpdateProfileRequest {
--- backend/vehicle-service-backend/src/main/java/com/vsc/vehicle_service_backend/entity/User.java
+++ backend/vehicle-service-backend/src/main/java/com/vsc/vehicle_service_backend/entity/User.java
@@ -1,13 +1,9 @@
-// User.java
 package com.vsc.vehicle_service_backend.entity;
 
 import jakarta.persistence.*;
-import lombok.Data;
 
 @Entity
 @Table(name = "users")
-@Data
-@ToString(exclude = "profile")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -29,4 +25,86 @@ public class User {
 
     @Column(nullable = false)
     private String role = "USER";
+
+    // Constructors
+    public User() {
+    }
+
+    public User(String firstName, String lastName, String email, String password, String phone, String role) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+        this.role = role;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
 }
